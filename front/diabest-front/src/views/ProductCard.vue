7068fe358b14cad8fdcddafda1849ae0d9564a89
Alexey Zhuravlev <a.zhuravlev@cdnvideo.com>
2022-05-02 01:28:08 +0300

    Add front files

---
+++ front/diabest-front/src/views/ProductCard.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'ProductCard',
+
+  data: () => ({
+  }),
+  components: {
+  },
+  created () {
+    console.log(this.$router.currentRoute)
+  },
+  computed: {
+    isHeaderNeed () {
+      return !(this.$router.currentRoute.name === 'Registration')
+    }
+  }
+};
+</script>
+
+<template lang="pug">
+
+div.container-limits
+  v-img(lazy-src='https://picsum.photos/id/11/10/6' max-height='300' max-width='250' src='https://picsum.photos/id/11/500/300')
+  span 'Те1кст'
+  a 'сСЫЛКА'
+</template>
+
+
+<style>
+
+
+
+.container-limits {
+  height: 450px;
+  width: 250px;
+}
+
+</style>
