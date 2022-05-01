3979f15683c0e3db4ec07b0814e32a91837103ce
Alexey Zhuravlev <a.zhuravlev@cdnvideo.com>
2022-05-02 01:08:27 +0300

    Add config file, removed config lines from main

---
+++ telegram/config.py
@@ -0,0 +1,4 @@
+token = '5220811515:AAEpZNfuq8dV_9xG31jZWxayZG4BGb15lKg'
+password = "4KJFD893K0-aKAoiu!4k9ipo54"
+register_url = 'http://127.0.0.1:5555/phone/code/'
+confirm_url = 'http://127.0.0.1:8080/registration/'
